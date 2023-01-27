4e9f520d1374105bbe140684e53b88cf1ee51a48
Geoffrey Mugford <65214363+NamidaSai@users.noreply.github.com>
2023-01-27 15:18:24 +0000

    Implement New Asteroids-like Movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -37,32 +37,11 @@ namespace Player
         private void FixedUpdate()
         {
             mover.Move(moveThrottle);
-            LookAt();
-        }
-
-        private void LookAt()
-        {
-            if (Gamepad.current == null)
-            {
-                Vector2 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-                currentLookDirection = (mouseWorldPosition - (Vector2)transform.position).normalized;
-            }
-            else if (lookThrottle.magnitude > 0.1f)
-            {
-                currentLookDirection = lookThrottle;
-            }
-
-            transform.up = currentLookDirection;
         }
 
         private void OnMove(InputValue value)
         {
             moveThrottle = value.Get<Vector2>();
-        }
-
-        private void OnLook(InputValue value)
-        {
-            lookThrottle = value.Get<Vector2>();
         }
 
         public void OnFire(InputValue value)
--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -4,36 +4,64 @@ namespace Player
 {
     public class PlayerMover : MonoBehaviour
     {
-        [SerializeField] float moveSpeed = 6f;
+        [SerializeField] private float moveSpeed = 6f;
+        [SerializeField] private float rotationSpeed = 5f;
+        [SerializeField] private float maxMoveAcceleration = 1f;
+        [SerializeField] private ParticleSystem propelFX;
+        [SerializeField] private SpriteRenderer propelSprite;
 
-        float startMoveSpeed;
         private Rigidbody2D thisRigidbody;
+        private Animator animator;
 
         private void Awake()
         {
-            startMoveSpeed = moveSpeed;
             thisRigidbody = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
         }
 
         public void Move(Vector2 moveThrottle)
         {
-            float currentMoveSpeed = moveSpeed;
+            if (moveThrottle.x != 0f)
+            {
+                Rotate(moveThrottle.x);
+            }
 
-            float moveSpeedX = moveThrottle.x * Time.fixedDeltaTime * currentMoveSpeed;
-            float moveSpeedY = moveThrottle.y * Time.fixedDeltaTime * currentMoveSpeed;
-            Vector2 moveForce = new Vector2(moveSpeedX, moveSpeedY);
-
-            thisRigidbody.velocity = moveForce;
+            if (moveThrottle.y != 0f)
+            {
+                Propel(moveThrottle.y);
+            }
+            else
+            {
+                propelFX.Stop();
+                propelSprite.enabled = true;
+            }
         }
 
-        public void SetMoveSpeed(float amount)
+        private void Rotate(float moveThrottleX)
         {
-            moveSpeed = amount;
+            Transform thisTransform = transform;
+            Vector2 position = thisTransform.position;
+            Vector2 turnThrottle = (Vector2)thisTransform.right * moveThrottleX;
+            Vector2 targetDirection = position + turnThrottle;
+            Vector2 desiredDirection = (targetDirection - position).normalized;
+            float angle = Mathf.Atan2(desiredDirection.y, desiredDirection.x) * Mathf.Rad2Deg - 90f;
+            Quaternion lookRotation = Quaternion.AngleAxis(angle, thisTransform.forward);
+            thisTransform.rotation = Quaternion.Slerp(thisTransform.rotation, lookRotation, Time.deltaTime * rotationSpeed); 
         }
 
-        public void ResetMoveSpeed()
+        private void Propel(float moveThrottleY)
         {
-            moveSpeed = startMoveSpeed;
+            Vector2 desiredDirection = transform.up;
+            Vector2 desiredVelocity = desiredDirection * moveSpeed;
+        
+            Vector2 steer = desiredVelocity - thisRigidbody.velocity;
+            float maxMoveForce = thisRigidbody.mass * maxMoveAcceleration;
+            steer = Vector2.ClampMagnitude(steer, maxMoveForce);
+
+            thisRigidbody.AddForce(steer);
+
+            propelFX.Play();
+            propelSprite.enabled = false;
         }
     }
 }
